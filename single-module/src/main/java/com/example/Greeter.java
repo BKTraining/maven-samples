639849060ece12c563214f7e71d33195830fc530
nlavoisier <nlavoisier@gmail.com>
2017-01-10 17:00:09 +0100

    Update Greeter.java
    change comment to improve code quality

--- single-module/src/main/java/com/example/Greeter.java
+++ single-module/src/main/java/com/example/Greeter.java
@@ -14,6 +14,7 @@ public class Greeter {
 
  /**
    * This is a greeting method
+   * @param someone is the name of the person
    */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
